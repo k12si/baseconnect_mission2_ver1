9728421aa26a7df17bca977b551e9c57224fd801
k12si <kakushin.nishimura@nislab.doshisha.ac.jp>
2018-10-09 20:52:05 +0900

    サーバエラー修正

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,3 +1,4 @@
 require 'redis'
-uri = URI.parse(ENV["REDIS_URL"])
-REDIS = Redis.new(host: uri.host, port: uri.port)
+# uri = URI.parse(ENV["REDIS_URL"])
+# uri = URI.parse("localhost:6379")
+REDIS = Redis.new(url: ENV["REDIS_URL"])
